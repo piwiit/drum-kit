2004b4b227e2cb906ac80912e19fa9418708ab51
piwiit <bouillaux@hotmail.fr>
2020-12-30 16:06:20 +0100

    add remove css transform after event

--- essais.js
+++ essais.js
@@ -4,6 +4,13 @@ window.addEventListener("keydown", function (e) {
   if (!audio) return; // stop the function from running all together
   audio.currentTime = 0; //rewind to the start
   audio.play();
-  console.log(key);
   key.classList.toggle("playing");
 });
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return; //skip it if it's not a transform
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
