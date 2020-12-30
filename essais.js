b7316c720cf417492c3c7dbcea0237be31a00a25
piwiit <bouillaux@hotmail.fr>
2020-12-30 16:12:29 +0100

    refactoring code

--- essais.js
+++ essais.js
@@ -1,11 +1,11 @@
-window.addEventListener("keydown", function (e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if (!audio) return; // stop the function from running all together
   audio.currentTime = 0; //rewind to the start
   audio.play();
   key.classList.toggle("playing");
-});
+}
 
 function removeTransition(e) {
   if (e.propertyName !== "transform") return; //skip it if it's not a transform
@@ -14,3 +14,4 @@ function removeTransition(e) {
 
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
